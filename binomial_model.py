da98876834fb7f7d218606f3efb44b120a0181a0
Michel Kuhn <michel.kuhn@live.com>
2026-01-30 22:24:45 +0100

    Update binomial_model.py
    speed up calculations

--- binomial_model.py
+++ binomial_model.py
@@ -6,10 +6,6 @@ u = 2
 d = 1 / u
 r = 0.02
 
-def check_discrete_no_arbitrage(u, d, r):
-    return 0 < d < 1+r < u
-
-
 def flip_coins(times):
     flips = []
     for i in range(0, times):
@@ -19,49 +15,29 @@ def flip_coins(times):
     return flips
 
 
-def calculate_binomial_stock(steps, starting_price):
-    states = [starting_price]
-    flips = flip_coins(steps)
-    for flip in flips:
-        if flip == 0:
-            states.append(states[-1]*d)
-        elif flip == 1:
-            states.append(states[-1]*u)
-
-    return states
-
-
-def calculate_riskless(steps, starting_cash):
-    states = [starting_cash]
-    for i in range(0, steps):
-        states.append(states[-1]*(1+r))
-
-    return states
-
-
-def calculate_wealth(steps, starting_wealth, delta, stock_states, cash_states):
-    states = [starting_wealth]
-    for i in range(0, steps):
-        position = delta*stock_states[i] + delta*cash_states[i]
-        states.append(position)
-
-    return states
-
-
 def run_simulation(number_of_steps):
-    if check_discrete_no_arbitrage(u, d, r):
-        y = calculate_binomial_stock(number_of_steps, S_0)
-        z = calculate_riskless(number_of_steps, X_0)
-        p = calculate_wealth(number_of_steps, X_0, delta_0, y, z)
+    if 0 < d < 1+r < u:
+        flips = flip_coins(number_of_steps)
 
-        return [y, z, p]
+        states: list[list[float]] = [[S_0], [X_0], [X_0]]
+        for i in range(0, number_of_steps):
+            if flips[i] == 0:
+                states[0].append(states[0][-1] * d)
+            elif flips[i] == 1:
+                states[0].append(states[0][-1] * u)
+
+            states[1].append(states[1][-1] * (1+r))
+
+            states[2].append(delta_0 * states[0][-1] + (1 - delta_0) * states[1][-1])
+
+        return states
     else:
         print("Arbitrage in the model!")
         return []
 
 
-def plot_simulation(simulation, n):
-    x = np.arange(0, n + 1, 1)
+def plot_simulation(simulation, steps):
+    x = np.arange(0, steps + 1, 1)
     plt.plot(x, simulation[0])
     plt.plot(x, simulation[1])
     plt.plot(x, simulation[2])
@@ -79,9 +55,7 @@ def plot_simulation(simulation, n):
 n = 10
 S_0 = 1
 X_0 = 1
-delta_0 = 0.5
+delta_0 = 0.3
 
 simulation_results = run_simulation(n)
 plot_simulation(simulation_results, n)
-
-
